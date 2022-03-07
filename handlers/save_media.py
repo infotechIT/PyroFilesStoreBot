a55d82cccc956f5f414bf1853c7a637ab087078f
InfoTech <infotechtele@gmail.com>
2022-03-07 17:46:25 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -50,15 +50,15 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{str_to_b64(str(SaveMessage.message_id))}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=infotechIT_{str_to_b64(str(SaveMessage.message_id))}"
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
             f"Just Click the link to get your files!",
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/JoinOT")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://t.me/infotechIT"),
+                  InlineKeyboardButton("Support Group", url="https://t.me/infogroup_tele")]]
             ),
             disable_web_page_preview=True
         )
@@ -98,8 +98,8 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/JoinOT")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://t.me/infotechIT"),
+                  InlineKeyboardButton("Support Group", url="https://t.me/infogroup_tele")]]
             ),
             disable_web_page_preview=True
         )
